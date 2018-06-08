cd3135e4a41415e6fb5cba51e36c71af730bf704
800147 <800147@gmail.com>
2018-06-08 20:35:33 +0300

    log server

--- src/store.js
+++ src/store.js
@@ -29,4 +29,14 @@ const store = {
   }
 };
 
-module.exports = { store };
+const log = (room, message) => {
+  room.logs.push({
+    users: Object.assign({}, room.users),
+    state: Object.assign({}, room.state),
+    message: message,
+    time: new Date()
+  });
+  console.log(room.logs);
+};
+
+module.exports = { store, log };
